0525574e91a02504a48e7a5797ae052588bc6a76
oskolskikh <oskolskikh@gmail.com>
2013-12-03 15:10:42 +0200

    added js files backbone and underscore

--- templates/main/main.php
+++ templates/main/main.php
@@ -1,21 +1,16 @@
-<?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="en" />
 
-        <!-- blueprint CSS framework -->
         <link rel="stylesheet" type="text/css" href="/css/screen.css" media="screen, projection" />
         <link rel="stylesheet" type="text/css" href="/css/print.css" media="print" />
-        <!--[if lt IE 8]>
-        <link rel="stylesheet" type="text/css" href="/css/ie.css" media="screen, projection" />
-        <![endif]-->
-
         <link rel="stylesheet" type="text/css" href="/css/main.css" />
         <link rel="stylesheet" type="text/css" href="/css/form.css" />
-
-        <title>JTS</title>
+        <script type="text/javascript" src="/js/underscore-min.js"></script>
+        <script type="text/javascript" src="/js/backbone-min.js"></script>
+        <script type="text/javascript" src="/js/freight_tools.js"></script>
+        <title>freight tool</title>
     </head>
 
     <body>
@@ -26,15 +21,7 @@
                     <?php $this->include_tpl("head_menu");?>
             </div><!-- mainmenu -->
             <div id="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a venenatis arcu, ac lacinia libero. Etiam at nisi quis nibh tempus laoreet. Mauris et aliquet odio, ac pretium magna. Vestibulum dolor tellus, lacinia sed nunc ac, consectetur fermentum nulla. Praesent sed est sapien. Ut ornare malesuada venenatis. Vivamus ac suscipit lacus, id dapibus enim. Aliquam ac diam sed ligula luctus consequat. Morbi scelerisque tempor arcu ut lacinia. Duis eu tristique est, nec scelerisque leo. Aliquam tristique ultricies neque, ac volutpat purus scelerisque eu. Nulla vel ante in elit bibendum porta non sit amet ante. Donec a risus enim. Vivamus faucibus sagittis mi, sed mollis eros facilisis et.
-
-Praesent elementum pellentesque ipsum, ut consectetur nibh eleifend cursus. Suspendisse at justo ac arcu imperdiet ultricies. Aliquam porttitor nunc nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum mollis, elit at luctus malesuada, felis ligula convallis mauris, ac sagittis nunc arcu vestibulum dolor. Etiam ornare tincidunt dapibus. Nunc a eros nec ipsum ornare tincidunt. Pellentesque at consequat nulla. Aliquam non purus a nunc tincidunt lobortis. Mauris augue enim, hendrerit ut posuere quis, ornare non leo. Aenean dignissim, ligula ac porta vulputate, justo massa iaculis risus, eu rhoncus dolor odio sit amet leo. Quisque iaculis leo eu lorem luctus vehicula. Donec lacinia tincidunt nunc, id ultricies enim ultrices at. Phasellus ac sapien congue, aliquam massa eget, congue augue.
-
-Sed imperdiet erat eu elementum bibendum. Vestibulum fermentum eros nisi, et imperdiet mauris tempus vitae. In lacus enim, placerat vitae feugiat consequat, volutpat vel elit. Aenean non erat sit amet orci tristique accumsan in a dolor. Sed eu nibh orci. Nulla sed semper sem. Donec et blandit nunc. Nam egestas euismod lorem a cursus. Vivamus ac sodales lorem.
-
-Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris condimentum luctus turpis, et bibendum dui lacinia sed. Phasellus a faucibus odio, vitae gravida neque. Nam sit amet elementum nulla, id lobortis enim. Quisque et massa euismod, lacinia turpis et, ornare lorem. Nulla lacinia at tellus a feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut sit amet lobortis neque. Pellentesque sit amet mauris accumsan, tristique eros vel, aliquam dui. Suspendisse non lobortis tellus. Praesent vulputate mollis feugiat. In sit amet libero scelerisque, faucibus ante vel, bibendum risus.
-
-In semper viverra dui et vulputate. Quisque dictum vehicula posuere. Nunc in neque eu neque tristique interdum. Vivamus viverra aliquam libero, nec placerat orci ultrices ut. Nulla dignissim nisi purus, vel convallis eros sollicitudin vitae. Vestibulum massa dui, facilisis lobortis mattis in, sollicitudin non lacus. Aliquam eget nibh nisi. Etiam rhoncus nunc purus, vel gravida felis accumsan eu. Praesent accumsan ultrices leo in vehicula. Cras ac magna ullamcorper sem aliquet vehicula. Nulla laoreet dolor convallis turpis dapibus, eget aliquam libero malesuada. Proin nec urna lectus. Proin dignissim libero sem, sed tincidunt nulla porta vel.
+                
             </div>
             
             <div class="clear"></div>
